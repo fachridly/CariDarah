03aa07f3b7716755cd7294f51e457962e80360f3
Fachri Fadly <fachridly@gmail.com>
2019-01-27 18:17:44 +0700

    Integrate Firebase and Sign In
    [Function]
    + integrate to firebase authentication, realtime database, messaging
    + sign in firebase
    + check if admin or not

--- app/src/main/java/com/exmth/caridarah/LoginActivity.java
+++ app/src/main/java/com/exmth/caridarah/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.exmth.caridarah;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends BaseActivity {
+    private final static String TAG = LoginActivity.class.getSimpleName();
+
     private TextInputEditText mEmailEditText, mPasswordEditText;
     private Button mRegisterButton, mLoginButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
 
         mRegisterButton.setOnClickListener(registerClickListener());
         mLoginButton.setOnClickListener(loginClickListener());
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void initializeView() {
@@ -48,15 +68,60 @@ public class LoginActivity extends AppCompatActivity {
                 String email = mEmailEditText.getText().toString();
                 String password = mPasswordEditText.getText().toString();
 
-                Intent intent;
-                if (email.equalsIgnoreCase("admin")) {
-                    intent = new Intent(LoginActivity.this, AdminMainActivity.class);
-                } else {
-                    intent = new Intent(LoginActivity.this, MainActivity.class);
+                if (isValidate(email, password)) {
+                    showProgressDialog();
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d(TAG, "sign in success");
+                                        final FirebaseUser user = mAuth.getCurrentUser();
+
+                                        DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("admin");
+                                        database.addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                                Intent intent = null;
+                                                for (DataSnapshot list : dataSnapshot.getChildren()) {
+                                                    if (list.getKey().equals(user.getUid())) {
+                                                        intent = new Intent(LoginActivity.this, AdminMainActivity.class);
+                                                        break;
+                                                    } else {
+                                                        intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                    }
+                                                }
+                                                hideProgressDialog();
+                                                if (intent != null) startActivity(intent);
+                                                finish();
+                                            }
+
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                                Log.d(TAG, "onCancelled: database admin");
+                                                hideProgressDialog();
+                                            }
+                                        });
+                                    } else {
+                                        Log.d(TAG, "sign in failure");
+                                        hideProgressDialog();
+                                        Toast.makeText(LoginActivity.this, "Terdapat kesalahan pada koneksi, email, atau password anda.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
-                startActivity(intent);
-                finish();
             }
         };
+    }
+
+    private boolean isValidate(String email, String password){
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Isi email terlebih dahulu", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (password.isEmpty()) {
+            Toast.makeText(this, "Isi password terlebih dahulu", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
